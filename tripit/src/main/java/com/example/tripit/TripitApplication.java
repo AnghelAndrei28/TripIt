7ce701a7dd2b569d9d2622c145ffdceb7a286784
Anghel Andrei <andrei.anghel2810@gmail.com>
2024-02-19 21:48:15 +0200

    Revert "Add Registration and Auth endpoints"

--- tripit/src/main/java/com/example/tripit/TripitApplication.java
+++ tripit/src/main/java/com/example/tripit/TripitApplication.java
@@ -1,7 +1,11 @@
 package com.example.tripit;
 
+import com.example.tripit.persistance.User;
+import com.example.tripit.persistance.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TripitApplication {
